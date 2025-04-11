7f927ca584cc0fc41ae2f7370fce67f5bb94447a
Devspace <153409985+Devspacetechnologies@users.noreply.github.com>
2025-04-11 23:59:06 +0300

    Create mark.js

---
+++ mark.js
@@ -0,0 +1,13 @@
+{
+  "name": "whatsapp-session-gen",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.6.5",
+    "express": "^4.18.2",
+    "qrcode-terminal": "^0.12.0"
+  }
+}
